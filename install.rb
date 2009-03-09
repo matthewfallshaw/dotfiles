20008edd0d5f0ed65576b73c2a2858564c103ef7
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2009-03-09 17:33:54 +1100

    support .bash_secrets so that secrets in public docs can be added later

--- install.rb
+++ install.rb
@@ -2,17 +2,36 @@
 
 # (originally) from http://errtheblog.com/posts/89-huba-huba
 # (I took this from git://github.com/mislav/dotfiles.git)
+# modified to support secrets appended after install
+#   secrets in ~/.dotfiles_secrets like
+#   gitconfig:
+#     "search_term": replace_term
+#     "other_search_term": other_replace_term
+
+def copy_and_replace_secrets(file, target, secrets)
+  %w[rubygems facets/shellwords].each {|l| require l }
+  system %[cp #{file} #{target}]
+  secrets[file].each do |search_term, replace_term|
+    system %[ruby -pi -e 'gsub(/#{Shellwords.escape(search_term)}/, "#{Shellwords.escape(replace_term)}")' #{target}]
+  end
+  puts "copy and update .#{file}"
+end
+def symlink(file, target)
+  system %[ln -vsf #{File.join(Dir.pwd, file)} #{target}]
+end
 
 home = ENV['HOME']
+require 'yaml'
+secrets = YAML.load(open(File.expand_path('~/.dotfiles_secrets')))
 
 Dir.chdir File.dirname(__FILE__) do
-  dotfiles_dir = Dir.pwd.sub(home + '/', '')
-  
   Dir['*'].each do |file|
-    next if file == 'install.rb'
+    next if file.match(/^(install.rb|utils|vendor)$/)
     target = File.join(home, ".#{file}")
-    unless File.exist? target
-      system %[ln -vsf #{File.join(dotfiles_dir, file)} #{target}]
+    if secrets[file]
+      copy_and_replace_secrets(file, target, secrets)
+    elsif !File.exist?(target)
+      symlink(file, target)
     end
   end
 end
