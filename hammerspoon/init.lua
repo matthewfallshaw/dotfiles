47536d49cf1b0de4a679459052ab83922674643b
Matthew Fallshaw <m@fallshaw.me>
2017-05-21 14:51:30 +1000

    Hammerspoon Oh-my-hammerspoon
    To be migrated to Spoons

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -53,4 +53,28 @@ spoon.Caffeine:bindHotkeys({toggle={{"⌘", "⌥", "⌃", "⇧"}, "c"}})
 spoon.Caffeine:start()
 
 hs.loadSpoon("Hermes")
+
+
+-- oh my hammerspoon
+require("oh-my-hammerspoon")
+omh_go({
+      -- "apps.hammerspoon_toggle_console",
+      -- "apps.hammerspoon_install_cli",
+      -- "apps.hammerspoon_config_reload",
+      -- "windows.manipulation",
+      -- "windows.grid",
+      "apps.skype_mute",
+      -- "mouse.locator",
+      "audio.headphones_watcher",
+      -- "misc.clipboard",
+      -- "misc.colorpicker",
+      -- "keyboard.menubar_indicator",
+      -- "apps.universal_archive",
+      -- "apps.universal_omnifocus",
+      -- "windows.screen_rotate",
+      "apps.evernote",
+       })
+
+
+
 hs.alert.show('Config loaded')
