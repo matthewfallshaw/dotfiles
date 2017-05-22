9652b24d8c4a37934d6abdb42ee0177c5b54ed3f
Matthew Fallshaw <m@fallshaw.me>
2017-05-22 18:44:42 +1000

    More Spoons

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,7 +7,7 @@ function usbDeviceCallback(data)
         if (data["eventType"] == "added") then
             hs.application.launchOrFocus("ScanSnap Manager")
         elseif (data["eventType"] == "removed") then
-            app = hs.appfinder.appFromName("ScanSnap Manager")
+            local app = hs.appfinder.appFromName("ScanSnap Manager")
             app:kill()
         end
     end
@@ -16,43 +16,44 @@ usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
 usbWatcher:start()
 
 
--- Reload config when any lua file in config directory changes
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == '.lua' then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-local myWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
-
-
 -- Spoons
 hs.loadSpoon("URLDispatcher")
 spoon.URLDispatcher.default_handler = "com.google.Chrome"
 spoon.URLDispatcher.url_patterns = {
-  { "url pattern", "application bundle ID" },
+  -- { "url pattern", "application bundle ID" },
   { "https?://www.pivotaltracker.com/.*", "com.fluidapp.FluidApp.PivotalTracker" },
-  { "https?://morty.trikeapps.com/.*", "org.epichrome.app.Morty" },
-  { "https?://app.asana.com/.*", "org.epichrome.app.Asana" },
+  { "https?://morty.trikeapps.com/.*",    "org.epichrome.app.Morty" },
+  { "https?://app.asana.com/.*",          "org.epichrome.app.Asana" },
 }
 spoon.URLDispatcher:start()
 
 hs.loadSpoon("Emojis")
-spoon.Emojis:bindHotkeys({ toggle={{"⌘", "⌥", "⌃", "⇧"}, "space"}, })
+spoon.Emojis:bindHotkeys({ toggle=  {{"⌘", "⌥", "⌃", "⇧"}, "space"}, })
 
 hs.loadSpoon("MouseCircle")
 spoon.MouseCircle:bindHotkeys({show={{"⌘", "⌥", "⌃", "⇧"}, "m"}})
 
 hs.loadSpoon("Caffeine")
-spoon.Caffeine:bindHotkeys({toggle={{"⌘", "⌥", "⌃", "⇧"}, "c"}})
+spoon.Caffeine:bindHotkeys({toggle= {{"⌘", "⌥", "⌃", "⇧"}, "c"}})
 spoon.Caffeine:start()
 
 hs.loadSpoon("Hermes")
+spoon.Hermes:bindHotkeys( {
+  playpause={{"⌥", "⌃", "⇧"}, "p"},
+  next=     {{"⌥", "⌃", "⇧"}, "n"},
+  like=     {{"⌥", "⌃", "⇧"}, "l"},
+  dislike=  {{"⌥", "⌃", "⇧"}, "d"},
+  tired=    {{"⌥", "⌃", "⇧"}, "t"},
+  hide=     {{"⌥", "⌃", "⇧"}, "h"},
+  quit=     {{"⌥", "⌃", "⇧"}, "q"},
+})
+
+hs.loadSpoon("Hammer")
+spoon.Hammer:bindHotkeys({
+  config_reload= {{"⌘", "⌥", "⌃", "⇧"}, "r"},
+  toggle_console={{"⌘", "⌥", "⌃", "⇧"}, "h"},
+})
+spoon.Hammer:start()
 
 
 -- oh my hammerspoon
