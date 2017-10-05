b6fc889afbf175e19c792651f1a6ceb07447f8b9
Matthew Fallshaw <m@fallshaw.me>
2017-10-05 07:04:26 -0700

    Cleanup and generalise.

--- hammerspoon/app-hotkeys.lua
+++ hammerspoon/app-hotkeys.lua
@@ -1,41 +1,50 @@
+-- App hotkeys
+--
+-- Add hs.hotkey.new (not .bind) to M.hotkeys["<hs.application:name()>"]. Add
+--   app_hotkeys = require 'app-hotkeys'
+--   app_hotkeys:start()
+-- to your init.lua and they will be enabled whenever the app has focus and
+-- disabled when it loses focus.
+
 local logger = hs.logger.new("App Hotkeys")
 
 local M = { hotkeys = {}, window_filters = {} }
 setmetatable(M.hotkeys, { __index = function(t,k) t[k]={}; return t[k] end }) -- initialise M.hotkeys.? = {}
 
 
--- Terminal hotkeys ⌘1-9 to Tab focus
+-- Terminal ⌘1-9 to tab focus
 logger.i("Terminal hotkeys for switching ⌘1-9 to Tab focus")
 for i=1,8 do
   -- Note: Attempting to raise a non-existent tab will cause an Applescript error which
   -- will be harmlessly ignored.
-  M.hotkeys.terminal[i] = hs.hotkey.new('⌘', tostring(i), function()
+  M.hotkeys["Terminal"][i] = hs.hotkey.new('⌘', tostring(i), function()
     hs.osascript.applescript('tell application "Terminal" to set selected of tab ' .. i .. ' of first window to true')
   end)
 end
-M.hotkeys.terminal[9] = hs.hotkey.new('⌘', "9", function()
+M.hotkeys["Terminal"][9] = hs.hotkey.new('⌘', "9", function()
   hs.osascript.applescript('tell application "Terminal" to set selected of last tab of first window to true')
 end)
 
 
-local function appname(app)
-  local appname = app:gsub("^%l", string.upper)
-  return appname
+
+-- Utility functions
+local function windowFocused(window, application_name, event)
+  for _,hotkey in pairs(M.hotkeys[application_name]) do hotkey:enable() end
+  logger.i(application_name .. " hotkeys enabled")
+end
+local function windowUnfocused(window, application_name, event)
+  for _,hotkey in pairs(M.hotkeys[application_name]) do hotkey:disable() end
+  logger.i(application_name .. " hotkeys disabled")
 end
 
-
+-- Module methods
 function M:start()
   for app,_ in pairs(self.hotkeys) do
-    self.window_filters[app] = hs.window.filter.new(appname(app))
+    self.window_filters[app] = hs.window.filter.new(app)
     self.window_filters[app]:subscribe({
-      [hs.window.filter.windowFocused] = function()
-        for _,hotkey in pairs(M.hotkeys[app]) do hotkey:enable() end
-        logger.i(app .. " hotkeys enabled")
-      end,
-      [hs.window.filter.windowUnfocused] = function()
-        for _,hotkey in pairs(M.hotkeys[app]) do hotkey:disable() end
-        logger.i(app .. " hotkeys disabled")
-      end
+      [hs.window.filter.windowFocused] = windowFocused,
+      [hs.window.filter.windowVisible] = windowFocused, -- because focusing a hidden app doesn't trigger windowFocused
+      [hs.window.filter.windowUnfocused] = windowUnfocused,
     })
   end
 end
