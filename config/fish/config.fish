4be799a99b40f0e81c669e4d56526058b7d2c166
Matthew Fallshaw <m@fallshaw.me>
2017-10-30 09:34:02 +1100

    … if only I knew what I was doing here… (I’m not loving the Fish Shell experience.)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,10 @@
 # set
 set -gx EDITOR vim
 set -gx fish_key_bindings fish_vi_key_bindings
-set -gx PATH ~/bin /usr/local/bin $PATH
+test -d /usr/local/sbin; and set -gx PATH /usr/local/sbin $PATH
+test -d /usr/local/bin; and set -gx PATH /usr/local/bin $PATH
+test -d ~/bin; and set -gx PATH ~/bin $PATH
+undup PATH
 
 # Aliases
 source ~/.config/fish/aliases.fish
