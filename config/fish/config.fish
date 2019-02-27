720b6b4c76cd99b29c320f8a66a32436cb8868ce
Matthew Fallshaw <m@fallshaw.me>
2019-02-28 09:47:00 +1100

    I still can't quite not hate fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,17 +2,11 @@ set fish_greeting  # nohello
 
 # set
 set -gx EDITOR vim
-test -d /usr/local/opt/findutils/libexec/gnubin; and set -gx PATH /usr/local/opt/findutils/libexec/gnubin $PATH
 test -d /usr/local/opt/findutils/libexec/gnuman; and set -gx MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
-test -d /usr/local/opt/coreutils/libexec/gnubin; and set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 test -d /usr/local/opt/coreutils/libexec/gnuman; and set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-test -d /usr/local/sbin; and set -gx PATH /usr/local/sbin $PATH
-test -d /usr/local/bin; and set -gx PATH /usr/local/bin $PATH
 test -d /usr/local/man; and set -gx MANPATH /usr/local/man $MANPATH
 test -d /usr/share/man; and set -gx MANPATH /usr/share/man $MANPATH
-test -d ~/bin; and set -gx PATH ~/bin $PATH
 
-set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
 for p in /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/{completion.fish.inc,path.fish.inc}
   test -e $p; and source $p
 end
@@ -21,7 +15,7 @@ if not functions -q fisher
     echo "Installing fisher for the first time..." >&2
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fisher
+    fisher -c fisher
 end
 
 function fish_prompt
@@ -37,3 +31,5 @@ test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell
 # undup PATH, MANPATH
 undup PATH
 undup MANPATH
+undup fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
