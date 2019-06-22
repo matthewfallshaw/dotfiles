00a6220c129cbc2d8cbdf56b04eb781dbbc40ed5
Matthew Fallshaw <m@fallshaw.me>
2019-06-22 19:42:04 +1000

    fish cleanup (we still hates it)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,16 +1,14 @@
-set fish_greeting  # nohello
-
-# paths
-## PATH
-set -gq PATH || set -gx PATH ''
-for d in (reverse /Users/matt/bin /Users/matt/code/clipboard-scripts/bin /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin /Users/matt/.rbenv/shims /usr/local/opt/sqlite/bin /usr/local/opt/fzf/bin /usr/local/MacGPG2/bin /opt/puppetlabs/bin /usr/local/opt/findutils/libexec/gnubin /usr/local/opt/coreutils/libexec/gnubin /usr/local/bin /usr/local/sbin /opt/X11/bin /usr/bin /usr/sbin /bin /sbin)
-  set -gx PATH $d $PATH
+# nohello
+if set -q fish_greeting
+  set -e fish_greeting
 end
 
 ## MANPATH
 set -gq MANPATH || set -gx MANPATH ''
 for d in (reverse /usr/local/opt/findutils/libexec/gnuman /usr/local/opt/coreutils/libexec/gnuman /usr/local/MacGPG2/share/man /opt/X11/share/man /usr/local/share/man /usr/local/man /usr/share/man)
-  set -gx MANPATH $d $MANPATH
+  if not contains $d $MANPATH
+    set -gx MANPATH $d $MANPATH
+  end
 end
 
 # google-cloud-sdk completion & path
@@ -34,19 +32,27 @@ if not functions -q fisher
   fisher -c fisher
 end
 
+# nvim
 if test -n "$NVIM_LISTEN_ADDRESS"
-	alias h "nvr -c 'doau BufEnter' -o"
-	alias v "nvr -c 'doau BufEnter' -O"
-	alias t "nvr -c 'doau BufEnter' --remote-tab"
-	alias o "nvr -c 'doau BufEnter'"
-	alias neovim 'command nvim'
-	alias nvim "echo 'You\'re already in nvim. Consider using o, h, v, or t instead. Use \'neovim\' to force.'"
+  alias h "nvr -c 'doau BufEnter' -o"
+  alias v "nvr -c 'doau BufEnter' -O"
+  alias t "nvr -c 'doau BufEnter' --remote-tab"
+  alias o "nvr -c 'doau BufEnter'"
+  alias neovim 'command nvim'
+  alias nvim "echo 'You\'re already in nvim. Consider using o, h, v, or t instead. Use \'neovim\' to force.'"
 else
   alias o 'nvim'
 end
 set -gx EDITOR 'nvim'
 set -gx SUDO_EDITOR nvim
 
+# pyenv
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
+
+# UI
 function fish_prompt
   powerline-shell --shell bare $status
 end
